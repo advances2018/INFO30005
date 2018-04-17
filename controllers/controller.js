292d86209bf69970b300c4b90e08ee3a2cf20e4c
Sam Wright <samrwright@Device.local>
2018-04-17 17:01:34 +1000

    updated to generate search page and search results dynamically

--- controllers/controller.js
+++ controllers/controller.js
@@ -37,9 +37,24 @@ module.exports.Indexdb = function(req, res){
 	res.render('view', { view : db[req.params.index] });
 };
 
+//function for generating the html for the results table
+function generateSearchTable(searchTerm){
+    var html = '<table class = "resultsTable" style="width:80%"><tbody>';
+    html += '<tr><td>Title 1</td><td>' + searchTerm + " test " + '0' + '</td><td class ="resultsTopRight">xxxx</td></tr>';
+    var i;
+    for (i = 1; i < 20; i++) { 
+        html += '<tr><td>Title ' + i + '</td><td>' + searchTerm + " test " + i + '</td><td>xxx</td></tr>';
+    }
+
+    html += '</tbody></table>';
+    return html;
+
+}
+
 module.exports.searchPage = function(req, res){
     var myVar = req.query.search; //mytext is the name of your input box
-    var searchResults = ["test1", "test2", "test3"];
+    var searchResults;
+    searchResults = generateSearchTable(myVar);
     //res.send('Your Text: ' +myText);
     res.render('pages/search', { myVar : myVar , searchResults : searchResults});
 };
